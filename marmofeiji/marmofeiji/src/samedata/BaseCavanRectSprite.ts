ecdeea8cd1122cba2ef48d51d393a4ae83816d31
sceneedit <52847457+zhaojianeng25@users.noreply.github.com>
2019-09-26 21:11:12 +0700

    同步

--- marmofeiji/marmofeiji/src/samedata/BaseCavanRectSprite.ts
+++ marmofeiji/marmofeiji/src/samedata/BaseCavanRectSprite.ts
@@ -116,9 +116,7 @@
                 Scene_data.context3D.setRenderTexture(this.shader, "s_texture", this._uvTextureRes.texture, 0);
  
                 Scene_data.context3D.setVc4fv(this.shader, "fColor", [0.999, 0, 0, 1]);
-
-     
-
+ 
                 Scene_data.context3D.drawCall(this.objData.indexBuffer, this.objData.treNum);
 
             }
