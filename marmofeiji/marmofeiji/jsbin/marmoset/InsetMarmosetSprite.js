7fbdb3eb7af70b80c7a92fc5063ed040e9b3f0f3
sceneedit <52847457+zhaojianeng25@users.noreply.github.com>
2019-10-05 14:32:28 +0700

    可用

--- marmofeiji/marmofeiji/jsbin/marmoset/InsetMarmosetSprite.js
+++ marmofeiji/marmofeiji/jsbin/marmoset/InsetMarmosetSprite.js
@@ -14,81 +14,69 @@ var __extends = (this && this.__extends) || (function () {
 var mars3D;
 (function (mars3D) {
     var Display3D = Pan3d.Display3D;
+    var Context3D = Pan3d.Context3D;
+    var InsetContext3D = /** @class */ (function (_super) {
+        __extends(InsetContext3D, _super);
+        function InsetContext3D(value) {
+            var _this = _super.call(this) || this;
+            _this.renderContext = value;
+            return _this;
+        }
+        return InsetContext3D;
+    }(Context3D));
+    mars3D.InsetContext3D = InsetContext3D;
     var InsetMarmosetSprite = /** @class */ (function (_super) {
         __extends(InsetMarmosetSprite, _super);
         function InsetMarmosetSprite() {
             return _super.call(this) || this;
         }
-        InsetMarmosetSprite.prototype.initBuffers = function (gl, shaderProgram) {
+        InsetMarmosetSprite.prototype.initBuffers = function (gl) {
             var vertices = new Float32Array([
-                0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+                -0.0, 0.5, -0.5, -0.5, 0.5, -0.5
             ]);
-            var n = 3; //点的个数
             //创建缓冲区对象
-            var vertexBuffer = gl.createBuffer();
-            if (!vertexBuffer) {
-                console.log("Failed to create the butter object");
-                return -1;
-            }
-            //将缓冲区对象绑定到目标
-            gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-            //向缓冲区写入数据
+            this.objData.vertexBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.objData.vertexBuffer);
             gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-            //获取坐标点
-            var a_Position = gl.getAttribLocation(shaderProgram, 'a_Position');
-            //将缓冲区对象分配给a_Position变量
-            gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-            //连接a_Position变量与分配给它的缓冲区对象
-            gl.enableVertexAttribArray(a_Position);
-            return n;
+            this.objData.treNum = 3;
         };
         InsetMarmosetSprite.prototype.makeInsetMesh = function () {
             if (!this._insetMarmose) {
-                //   this._insetMarmose = this;
-                //顶点着色器程序
+                this.objData = new ObjData;
+                // this._insetMarmose = this;
                 var VSHADER_SOURCE = "attribute vec4 a_Position;" +
                     "void main() {" +
-                    //设置坐标
                     "gl_Position = a_Position; " +
                     "} ";
-                //片元着色器
                 var FSHADER_SOURCE = "void main() {" +
-                    //设置颜色
                     "gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);" +
                     "}";
-                //获取canvas元素
-                //获取绘制二维上下文
                 var gl = this._gl;
-                if (!gl) {
-                    console.log("Failed");
-                    return;
-                }
-                //编译着色器
                 var vertShader = gl.createShader(gl.VERTEX_SHADER);
                 gl.shaderSource(vertShader, VSHADER_SOURCE);
                 gl.compileShader(vertShader);
                 var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
                 gl.shaderSource(fragShader, FSHADER_SOURCE);
                 gl.compileShader(fragShader);
-                //合并程序
-                var shaderProgram = gl.createProgram();
-                gl.attachShader(shaderProgram, vertShader);
-                gl.attachShader(shaderProgram, fragShader);
-                gl.linkProgram(shaderProgram);
-                gl.useProgram(shaderProgram);
-                //获取坐标点
-                var a_Position = gl.getAttribLocation(shaderProgram, 'a_Position');
-                if (a_Position < 0) {
-                    console.log('Failed to get the storage location of a_Position');
-                    return;
-                }
-                var n = this.initBuffers(gl, shaderProgram);
-                gl.drawArrays(gl.TRIANGLES, 0, n);
+                this.program = gl.createProgram();
+                gl.attachShader(this.program, vertShader);
+                gl.attachShader(this.program, fragShader);
+                gl.linkProgram(this.program);
+                this.initBuffers(gl);
             }
         };
         InsetMarmosetSprite.prototype.upDataBygl = function (value) {
             this._gl = value;
+            if (!this._context3D) {
+                this._context3D = new InsetContext3D(this._gl);
+            }
             this.makeInsetMesh();
+            var gl = this._gl;
+            gl.useProgram(this.program);
+            var temp = gl.getAttribLocation(this.program, 'a_Position');
+            gl.vertexAttribPointer(temp, 2, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(temp);
+            gl.drawArrays(gl.TRIANGLES, 0, this.objData.treNum);
         };
         return InsetMarmosetSprite;
     }(Display3D));
--- marmofeiji/marmofeiji/src/marmoset/InsetMarmosetSprite.ts
+++ marmofeiji/marmofeiji/src/marmoset/InsetMarmosetSprite.ts
@@ -1,68 +1,53 @@
 ﻿module mars3D {
  
     import Display3D = Pan3d.Display3D
+    import Context3D = Pan3d.Context3D
  
     
 
+    export class InsetContext3D extends Context3D {
+        constructor(value: WebGLRenderingContext) {
+            super();
+            this.renderContext = value;
+        }
+    }
     export class InsetMarmosetSprite extends Display3D {
-
+        private _context3D: InsetContext3D
         constructor() {
             super();
- 
+       
         }
         private _gl: WebGLRenderingContext
         private _insetMarmose: InsetMarmosetSprite
-        private initBuffers(gl: WebGLRenderingContext, shaderProgram: WebGLProgram): number {
+        private initBuffers(gl: WebGLRenderingContext): void {
             var vertices = new Float32Array([
-                0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+                -0.0, 0.5, -0.5, -0.5, 0.5, -0.5
             ]);
-            var n = 3;//点的个数
             //创建缓冲区对象
-            var vertexBuffer = gl.createBuffer();
-            if (!vertexBuffer) {
-                console.log("Failed to create the butter object");
-                return -1;
-            }
-            //将缓冲区对象绑定到目标
-            gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-            //向缓冲区写入数据
+            this.objData.vertexBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.objData.vertexBuffer);
             gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-            //获取坐标点
-            var a_Position = gl.getAttribLocation(shaderProgram, 'a_Position');
-            //将缓冲区对象分配给a_Position变量
-            gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-            //连接a_Position变量与分配给它的缓冲区对象
-            gl.enableVertexAttribArray(a_Position);
-            return n;
+            this.objData.treNum=3
+
+  
 
         }
         private makeInsetMesh(): void {
             if (!this._insetMarmose) {
-             //   this._insetMarmose = this;
-
-                //顶点着色器程序
+                this.objData = new ObjData;
+               // this._insetMarmose = this;
                 var VSHADER_SOURCE =
                     "attribute vec4 a_Position;" +
                     "void main() {" +
-                    //设置坐标
                     "gl_Position = a_Position; " +
                     "} ";
 
-                //片元着色器
                 var FSHADER_SOURCE =
                     "void main() {" +
-                    //设置颜色
                     "gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);" +
                     "}";
-                //获取canvas元素
-             
-                //获取绘制二维上下文
                 var gl = this._gl
-                if (!gl) {
-                    console.log("Failed");
-                    return;
-                }
-                //编译着色器
+      
                 var vertShader = gl.createShader(gl.VERTEX_SHADER);
                 gl.shaderSource(vertShader, VSHADER_SOURCE);
                 gl.compileShader(vertShader);
@@ -70,26 +55,14 @@
                 var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
                 gl.shaderSource(fragShader, FSHADER_SOURCE);
                 gl.compileShader(fragShader);
-                //合并程序
-                var shaderProgram = gl.createProgram();
-                gl.attachShader(shaderProgram, vertShader);
-                gl.attachShader(shaderProgram, fragShader);
-                gl.linkProgram(shaderProgram);
-                gl.useProgram(shaderProgram);
 
-                //获取坐标点
-                var a_Position = gl.getAttribLocation(shaderProgram, 'a_Position');
+                this.program = gl.createProgram();
+                gl.attachShader(this.program, vertShader);
+                gl.attachShader(this.program, fragShader);
+                gl.linkProgram(this.program);
 
-                if (a_Position < 0) {
-                    console.log('Failed to get the storage location of a_Position');
-                    return;
-                }
+                this.initBuffers(gl);
 
-                var n = this.initBuffers(gl, shaderProgram);
- 
-              
-
-                gl.drawArrays(gl.TRIANGLES, 0, n);
                
             }
 
@@ -99,10 +72,17 @@
     
         public upDataBygl(value: WebGLRenderingContext): void {
             this._gl = value;
-     
-
+            if (!this._context3D) {
+                this._context3D = new InsetContext3D(this._gl)
+            }
             this.makeInsetMesh()
-     
+
+            var gl = this._gl
+            gl.useProgram(this.program);
+            var temp = gl.getAttribLocation(this.program, 'a_Position')
+            gl.vertexAttribPointer(temp, 2, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(temp);
+            gl.drawArrays(gl.TRIANGLES, 0, this.objData.treNum);
       
         }
 
